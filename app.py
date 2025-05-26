585de8c940467a5edd57cc65adb143c9012960e3
Jay Tamkhane <jaytamkhane161@gmail.com>
2025-05-26 18:16:17 +0530

    templates/css  updates

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, render_template
 from src.pipelines.prediction_pipeline import PredictionPipeline, CustomData
 
 app = Flask(__name__)
@@ -7,30 +7,30 @@ app = Flask(__name__)
 def home_page():
     return render_template('index.html')
 
-@app.route('/predict', methods=['GET', 'POST'])
-def predict_datapoint():
-    if request.method == 'GET':
-        return render_template('index.html')
-    else:
-        data = CustomData(
-            carat=float(request.form.get('carat')),
-            depth=float(request.form.get('depth')),
-            table=float(request.form.get('table')),
-            x=float(request.form.get('x')),
-            y=float(request.form.get('y')),
-            z=float(request.form.get('z')),
-            cut=request.form.get('cut'),
-            color=request.form.get('color'),
-            clarity=request.form.get('clarity')
-        )
+@app.route('/predict', methods=['GET'])
+def show_form():
+    return render_template('predict.html')
 
-        final_new_data = data.get_data_as_dataframe()
-        prediction_pipeline = PredictionPipeline()
-        pred = prediction_pipeline.predict(final_new_data)
-        
-        results = round(pred[0], 2)
+@app.route('/result', methods=['POST'])
+def predict_result():
+    data = CustomData(
+        carat=float(request.form.get('carat')),
+        depth=float(request.form.get('depth')),
+        table=float(request.form.get('table')),
+        x=float(request.form.get('x')),
+        y=float(request.form.get('y')),
+        z=float(request.form.get('z')),
+        cut=request.form.get('cut'),
+        color=request.form.get('color'),
+        clarity=request.form.get('clarity')
+    )
 
-        return render_template('result.html', final_result=results)
+    final_new_data = data.get_data_as_dataframe()
+    prediction_pipeline = PredictionPipeline()
+    pred = prediction_pipeline.predict(final_new_data)
+    result = round(pred[0], 2)
+
+    return render_template('result.html', final_result=result)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)

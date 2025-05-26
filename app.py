ded1aa5ce20f31c403dff1c54202a7c7c656177e
Jay Tamkhane <jaytamkhane161@gmail.com>
2025-05-26 20:42:10 +0530

    kuch bhi

--- app.py
+++ app.py
@@ -33,5 +33,5 @@ def predict_result():
     return render_template('result.html', final_result=result)
 
 if __name__ == "__main__":
-    print("Server running at: http://127.0.0.1:5000")
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    print("Server running at: http://127.0.0.1:8080")
+    app.run(host='0.0.0.0', port=8080, debug=True)
